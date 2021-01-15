6699d53418c9ecfc3c55101e1106fd6879480642
John Bernhard <johnbernhard2@gmail.com>
2021-01-15 12:34:16 +0100

    ScreenManager hinzugefuegt

---
+++ Assets/Script/UI/StandardTools/ScreenManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ScreenManager : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<ScreenManager, UxmlTraits> { }
+
+    //public new class UxmlTraits : VisualElement.UxmlTraits { }
+}
